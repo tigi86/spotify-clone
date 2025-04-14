912e6cc51eafa12369442f79abcf3f5b9ea029bc
tigistu <twoo53591@gmail.com.com>
2025-04-14 10:59:46 +0300

    new

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
-import express from "express";
-import cors from "cors";
+// import express from "express";
+// import cors from "cors";
 import "dotenv/config";
 import songRouter from "./src/routes/songRoute.js";
 import connectDB from "./src/config/mogodb.js";
 import connectCloudinary from "./src/config/cloudinary.js";
 import albumRouter from "./src/routes/albumRoute.js";
 
-// app config
+const express = require("express");
+const cors = require("cors");
 const app = express();
+
+// ✅ Enable CORS for all domains (or specify only your frontend domain)
+app.use(
+  cors({
+    origin: "https://spotify-clone-frontend-ll6e.onrender.com",
+    credentials: true, // if you use cookies or auth
+  })
+);
+// app config
+// const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 connectCloudinary();
